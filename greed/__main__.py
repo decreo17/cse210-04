5476b428ad64f6f95daef9a679d4fc3d320564e4
wcamer <xeus.xiii@hotmail.com>
2022-06-09 14:45:51 -0600

    updated main.py and director

--- greed/__main__.py
+++ greed/__main__.py
@@ -1,7 +1,6 @@
 from gc import collect
 import os
 import random
-import arcade
 
 from game.casting.game_object import GameObject
 from game.casting.collection import Collection
@@ -13,7 +12,10 @@ from game.services.video_service import VideoService
 
 from game.shared.color import Color
 from game.shared.point import Point
+from game.shared.text_randomizer import Text_randomizer
 from game.casting.falling_object import FallingObject
+from game.casting.power_ups import Power_up
+
 
 FRAME_RATE = 12
 MAX_X = 900
@@ -21,77 +23,186 @@ MAX_Y = 600
 FLOOR = 580
 CELL_SIZE = 20
 FONT_SIZE = 20
+#below is a testing section
+banner_size = 107
+
+
 COLS = 60
 ROWS = 40
 CAPTION = "Greed"
-#DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
+
 WHITE = Color(255, 255, 255)
-DEFAULT_FALLING_OBJECTS = 40
+DEFAULT_FALLING_OBJECTS = 96 #original number is 40 
+ge = 0
+ro = 0
+pu = 0
+cur = 0
+rando = 0
+checks = 0
 
 
 def main():
     
+    power_up = Power_up()
+    
     # create the collection
     collection = Collection()
     
-    # create the banner
+    # start the game
+    keyboard_service = KeyboardService(CELL_SIZE)
+    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    director = Director(keyboard_service, video_service, collection,power_up)
+    
+    
+    # create the banner this is the score board
     banner = GameObject()
     banner.set_text("")
+    
+    #below is the original code and below that is the test code
     banner.set_font_size(FONT_SIZE)
+    #banner.set_font_size(banner_size) #test code
+    
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
     collection.add_game_object("banners", banner)
     
-    # create the player
+    # create the player the objecty that moves around.
     x = int(MAX_X / 2)
     y = FLOOR
+    #print('this is the floorrrrrrrrrrrrrrrrrrrrrrrrrr', FLOOR, 'this is the x', x, 'this is the y' , y) # debugging
     position = Point(x, y)
 
     player = GameObject()
     player.set_text("#")
+    
+    #original code below
     player.set_font_size(FONT_SIZE)
+    #player.set_font_size(banner_size)
+    
     player.set_color(WHITE)
     player.set_position(position)
     collection.add_game_object("players", player)
+      
+    if director._points < 100:
 
-    for obj in range(DEFAULT_FALLING_OBJECTS):
-        n = obj % 2
-        if(n == 0):
-            text = "*"
-        
-        if(n != 0):
-            text = "O"
-        print(f"this is {text}")
-        x = random.randint(1, MAX_X)
-        # y = FONT_SIZE
-        y = random.randint(1, MAX_Y)
-        position = Point(x, y)
-        #position = position.scale(CELL_SIZE)
+        for obj in range(DEFAULT_FALLING_OBJECTS): #in range of the total number of falling items
+            global checks
+            checks += 1
+            n = obj % 5
+            if(n == 0):
+                text = "gem" #this is the gem
+                global ge 
+                ge += 1
+                #global checks
+                #checks += 1
+                
+            elif(n == 1): #if (n !=0):  #this is the original
+                text = "ro" #this is the rock
+                global ro
+                ro += 1
+                #global checks
+                #checks += 1
+                
+                 
+            #the remainder of this if statement is experiemenal
+            elif(n == 2):
+                text = 'mult'
+                global pu
+                pu += 1
+                #global checks
+                #checks += 1
+                
+            elif(n == 3 ):
+                text = 'cur'
+                global cur
+                cur += 1
+                
+            
+            else:
+                text = 'ran'
+                global rando
+                rando += 1
+            
+            #the block below is experiemental
+            # if(n == 3):
+            #     text = 'power up'
+            #     global pu
+            #     pu += 1
+            
+            #the block below is the original.
+            # if(n == 1): #if (n !=0):  #this is the original
+            #     text = "O" #this is the rock
+            #     global r
+            #     r += 1
+                
+            print(f"this is {text}")
+            x = random.randint(1, MAX_X)
+            # y = FONT_SIZE
+            y = random.randint(1, MAX_Y)
+            position = Point(x, y)
+            #position = position.scale(CELL_SIZE)
 
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
-        
-        falling_object = FallingObject()
-        falling_object.set_text(text)
-        falling_object.set_font_size(FONT_SIZE)
-        falling_object.set_color(color)
-        falling_object.set_position(position)
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            color = Color(r, g, b)
+            point = Point
+            
+            
+            #falling objects that player will collect and avoid
+            falling_object = FallingObject()
+            falling_object.set_text(text)
+            falling_object.set_font_size(FONT_SIZE)
+            falling_object.set_color(color)
+            falling_object.set_position(position)
+            
+            
 
-        if (text == "O"):
-            falling_object.set_points(-1)
-        
-        if (text =="*"):
-            falling_object.set_points(2)
+            if (text == "ro"):
+                falling_object.set_points(-1)
+            
+            if (text =="gem"):
+                falling_object.set_points(2)
+                
+            if (text == "mult"):
+                base = 1 
+                falling_object.set_points(base)
+                print('you got a bonus')
+                
+            if text == 'cur':
+                negamult = 0
+                falling_object.set_points(negamult)
+                
+            if text == 'ran':
+                falling_object.set_points(1)
+                
+                
 
-        collection.add_game_object("falling_objects", falling_object)
+            collection.add_game_object("falling_objects", falling_object)
+            
+            
+                
+            
+                
+                
+    
 
-    # start the game
-    keyboard_service = KeyboardService(CELL_SIZE)
-    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
-    director = Director(keyboard_service, video_service, collection)
+
+    # # start the game
+    # keyboard_service = KeyboardService(CELL_SIZE)
+    # video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    # director = Director(keyboard_service, video_service, collection)
     director.start_game()
+    #print statements below are for debuggin 
+    print('the total number of checks',checks)
+    print('the starting total number for gems is',ge)
+    print('the starting total number for rocks is',ro)
+    print('the starting total number of powerup', pu)
+    print('the starting total number 0f curses', cur)
+    print('the starting total number of randomizer items is ', rando)
+    print('the starting total number of falling objects', DEFAULT_FALLING_OBJECTS)
+    print(f'the score ended at  {director._points: .0f}')
+    
 
 
 if __name__ == "__main__":
--- greed/game/directing/director.py
+++ greed/game/directing/director.py
@@ -1,5 +1,11 @@
 from game.shared.point import Point
+from game.shared.text_randomizer import Text_randomizer
+from game.casting.curses import Curses
+from game.casting.randomizer import Randomizer
+from game.casting.falling_object import FallingObject
 import random
+
+import time
 
 class Director:
     """A person who directs the game. 
@@ -11,7 +17,7 @@ class Director:
         _video_service (VideoService): For providing video output.
     """
 
-    def __init__(self, keyboard_service, video_service, collection):
+    def __init__(self, keyboard_service, video_service, collection, power_up):
         """Constructs a new Director using the specified keyboard and video services.
         
         Args:
@@ -27,7 +33,16 @@ class Director:
         self._video_service = video_service
         self._floor = 585
         self._collection = collection
-        self._points = 00
+        self._points = 0
+        self._power_up = power_up
+        self._curses = Curses()
+        self._randomizer = Randomizer()
+        self._rando_text = Text_randomizer()
+        #self._amp = 0
+        
+        self._falling_object = FallingObject()
+        
+        
         
     def start_game(self):
         """Starts the game using the given collection. Runs the main game loop.
@@ -46,7 +61,7 @@ class Director:
             collection (collection): The collection of actors.
         """
         player = collection.get_first_game_object("players")
-        velocity = self._keyboard_service.get_direction()
+        velocity = self._keyboard_service.get_direction() #this sets an x,y corridnate
         player.set_velocity(velocity)
 
     def _do_updates(self, collection):
@@ -55,25 +70,123 @@ class Director:
         Args:
             collection (collection): The collection of actors.
         """
+        rename = self._rando_text
+        mystery = self._randomizer
         banner = collection.get_first_game_object("banners")
         player = collection.get_first_game_object("players")
         falling_objects = collection.get_game_objects("falling_objects")
-
-        banner.set_text(f"Score: {self._points}")
+        
+        #print('your points are', self._points)
+        banner.set_text(f"Score: {self._points: .0f}")
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
-        player.move_next(max_x, max_y)
+        player.move_next(max_x , max_y)  
         
         for falling_object in falling_objects:
-            if player.distance(falling_object.get_position()) < 20:
-                self._points += falling_object.get_points()
-                collection.remove_game_object("falling_objects", falling_object)
-        
+            if player.distance(falling_object.get_position()) < 20: #collision detection
+                #this is if the player gets a multiplier power up
+                if falling_object.get_text() == "mult":
+                    #print('you got a mult, points before multttttttttttttttttttttttttttttttttttttt',self._points)
+                    #mult = (self._points * self._power_up.multiplier())#falling_object.get_points()) 
+                    self._points *= (falling_object.get_points() + self._power_up.multiplier()) 
+                    #print('points after mult',self._points)
+                    collection.remove_game_object("falling_objects", falling_object)
+                    
+                #if player gets a curse    
+                elif falling_object.get_text() == 'cur':
+                    #print('points before curseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',self._points)
+                    curse = (self._points *  self._curses.bad_mult()) 
+                    self._points += curse
+                    #print('points after curse',self._points)
+                    collection.remove_game_object('falling_objects', falling_object)
+                 
+                 
+                 
+                #if player gets a randomizer item    
+                elif falling_object.get_text() == 'ran':
+                    #print('points before randomizerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',self._points)
+                    result = mystery.mystery_box()
+                    print('this is the outcome of the mystery box', result)
+                    
+                    
+                    if result > 2: #this is if the mystery box just give a flat number increase
+                        print('points before flat mystery box',self._points)
+                        self._points += result
+                        print('points after randomizer',self._points)
+                        collection.remove_game_object('falling_objects', falling_object)
+                    
+                    elif result > 0 and result <= 2: #if mystery box give a positive multiplier
+                        print('this is a randomizer multiplier',result)
+                        print('points before good mystery box',self._points)
+                        self._points += (self._points * result)
+                        print('points after good mystery box',self._points)
+                        collection.remove_game_object('falling_objects', falling_object)
+                        
+                    elif result < 0:
+                        print('this is a negative multiplier from mystery box')
+                        print('points before bad mystery', self._points)
+                        self._points += (self._points * result)
+                        # ran = (self._points * self._randomizer.mystery_box())
+                        #   self._points += ran
+                        print('points after randomizer',self._points)
+                        collection.remove_game_object('falling_objects', falling_object)
+                
+                # can add future power ups here
+                    
+                
+                #if player gets a rock
+                elif falling_object.get_text() == 'ro':
+                    # print('you hit a rock')
+                    # print('points before rock', self._points)
+                    self._points += falling_object.get_points()
+                    #print('points after rock', self._points)
+                    collection.remove_game_object('falling_objects', falling_object)
+                    
+                # if not a power up, curse, or rock it is a gem
+                else:
+                    #print('you got a gemmmmmmmmmmmmm your old points',self._points)
+                    self._points += falling_object.get_points()
+                    #print('your points are nowwwwwwwwwwwwww',self._points)
+                    collection.remove_game_object('falling_objects', falling_object)
+                   
+            
+            
+                    
         #falling objects
         for falling_object in falling_objects: 
-            velocity = Point(0,random.randint(8,15))
-            falling_object.set_velocity(velocity)
+            velocity = Point(0,(random.randint(8,15)))# + self._amp )) # this will make the gems and rocks fall faster.  If the number is below zero it will go in reverse
+            falling_object.set_velocity(velocity) 
             falling_object.move_next(max_x, max_y)
+            
+            
+        # going through all the falling objects and if falling_object falls past 580...
+        # then it will get renamed, its value reset, starting position x coordinate randomized and y coordinate starting at the top of the screen.  
+        # After that it is again put into the list of falling objects
+        for falling_object in falling_objects:
+            #print(falling_object.get_position().get_y()) #this will get the y point debugging line
+            if falling_object.get_position().get_y() > 580:
+                #print('y is greater than the floor') # debugging
+                #print('old name for the object is.............', falling_object.get_text())
+                falling_object.set_text(rename.renamer())
+               # print('the new name is ', falling_object.get_text())
+                #print('here is the old value',falling_object.get_points())
+                falling_object.set_points(rename.value_setter())
+                #print('this is the new value',falling_object.get_points())
+                newx = random.randint(1,max_x)
+                newy = max_y
+                repost = Point(newx,newy)
+                falling_object.set_position(repost)
+                #print('new object incomingggggggggggggggggggggggggggg') # debugging
+                collection.add_game_object('falling_objects', falling_object)
+                #print('the new name for the object is.........',falling_object.get_text()) #debugging 
+        
+        
+            
+        
+                
+            
+        # if self._points >= 10:
+        #     banner.set_text("You Win")
 
     def _do_outputs(self, collection):
         """Draws the actors on the screen.
@@ -85,5 +198,4 @@ class Director:
         actors = collection.get_all_game_objects()
         self._video_service.draw_game_objects(actors)
         self._video_service.flush_buffer()
-
         
